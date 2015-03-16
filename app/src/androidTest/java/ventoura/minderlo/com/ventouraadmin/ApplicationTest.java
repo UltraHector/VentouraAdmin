33b015b4e9c1ba99b1bd26992027b1daa696c216
Hector <hector@ventoura.net>
2015-03-16 21:24:45 +1000

    Initial commit

---
+++ app/src/androidTest/java/ventoura/minderlo/com/ventouraadmin/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ventoura.minderlo.com.ventouraadmin;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
